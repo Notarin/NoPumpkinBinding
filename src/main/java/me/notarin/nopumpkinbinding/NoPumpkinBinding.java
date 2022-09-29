08f1e446fd936356b836dc9c6a7d33021dcdd35e
Notarin <424c414e4b@gmail.com>
2022-09-29 19:30:01 -0400

    Added a configuration that gives the option between removing the enchantment, breaking, and dropping

--- src/main/java/me/notarin/nopumpkinbinding/NoPumpkinBinding.java
+++ src/main/java/me/notarin/nopumpkinbinding/NoPumpkinBinding.java
@@ -2,6 +2,7 @@ package me.notarin.nopumpkinbinding;
 
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
@@ -45,12 +46,25 @@ public final class NoPumpkinBinding extends JavaPlugin implements Listener {
         if (command.getName().equalsIgnoreCase("getthispumpkinoffme")) {
             if (sender instanceof Player) {
                 Player p = (Player) sender;
+                World w = p.getWorld();
                 try {
                     ItemStack i = p.getInventory().getHelmet();
                     assert i != null;
                     if (i.getType().name().contains("CARVED_PUMPKIN") && i.containsEnchantment(Enchantment.BINDING_CURSE)) {
-                        System.out.println("Removing curse of binding.");
-                        i.removeEnchantment(Enchantment.BINDING_CURSE);
+                        if (!config.getBoolean("drop")) {
+                            System.out.println("Removing curse of binding.");
+                            i.removeEnchantment(Enchantment.BINDING_CURSE);
+                        }
+                        else {
+                            if (!config.getBoolean("break")) {
+                                p.sendMessage("Dropping pumpkin.");
+                                w.dropItem(p.getLocation(), i);
+                            }
+                            else {
+                                p.sendMessage("Destroying pumpkin.");
+                            }
+                            i.setAmount(0);
+                        }
                     } else {
                         p.sendMessage(ChatColor.RED + "You are not wearing a pumpkin with the Curse of Binding!");
                     }
